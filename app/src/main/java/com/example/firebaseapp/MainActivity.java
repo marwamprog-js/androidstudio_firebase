d2385c99b4d01a8f28dbdaf3075a63bb083bfd8b
Marwam Malta <marwamprog.js@gmail.com>
2023-04-03 21:23:22 -0300

    Seção 16: Firebase 167. Recuperando dados

--- app/src/main/java/com/example/firebaseapp/MainActivity.java
+++ app/src/main/java/com/example/firebaseapp/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.firebaseapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,14 +21,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DatabaseReference usuarios = reference.child( "usuarios" );
+        DatabaseReference usuarios = reference.child( "usuarios" ).child("001");
+        DatabaseReference produtos = reference.child("produtos");
 
+        usuarios.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Log.i("FIREBASE", snapshot.getValue().toString() );
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
+        /*
         Usuario usuario = new Usuario();
         usuario.setNome("Marwam");
         usuario.setSobrenome("Malta");
         usuario.setIdade(33);
 
-        DatabaseReference produtos = reference.child("produtos");
+
 
         Produto produto = new Produto();
         produto.setDescricao("Iphone X");
@@ -34,6 +55,8 @@ public class MainActivity extends AppCompatActivity {
         usuarios.child("002").setValue( usuario );
         produtos.child("001").setValue( produto );
 
+
+         */
 
     }
 }
